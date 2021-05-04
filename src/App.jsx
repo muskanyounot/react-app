578ae03c896226d7d486c88cc8d70758a5df43b6
muskanyounot <79860048+muskanyounot@users.noreply.github.com>
2021-05-04 12:51:35 +0100

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from 'react';
+import "./App.css";
+import Kanye  from './components/kanye-api';
+
+export const App = () => {
+
+  return (
+    <Kanye />
+  )
+}
+export default App;
